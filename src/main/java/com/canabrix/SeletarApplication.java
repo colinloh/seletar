19e645b3572048b8aee1553e6e0eb0419ff64a72
Colin Loh <colinloh@hotmail.com>
2023-01-05 21:44:43 -0800

    Added mvn folder in

--- src/main/java/com/canabrix/SeletarApplication.java
+++ src/main/java/com/canabrix/SeletarApplication.java
@@ -1,20 +1,17 @@
 package com.canabrix;
 
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.canabrix.aws.CanabrixAwsService;
-import com.canabrix.wordle.WordleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpSession;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -26,49 +23,17 @@ public class SeletarApplication {
 		SpringApplication.run(SeletarApplication.class, args);
 	}
 
-	private static final String S3_WORDS_PATH = "words";
-
-	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss'Z'");
-
-	@GetMapping("/s3list")
-	public List<String> s3List() {
-		List<String> keys = new ArrayList<>();
-		awsService.listObjectSummaries(S3_WORDS_PATH).forEach(
-				s -> keys.add(s.getKey())
-		);
-		return keys;
-	}
-
-	@GetMapping("/s3words")
-	public List<String> s3Words(@RequestParam(name = "filename") String filename) throws IOException {
-		logger.info("Fetching: " + filename);
-		List<String> words = new ArrayList<>();
-		awsService.consumeS3Lines(
-				S3_WORDS_PATH + "/" + filename,
-				words::add
-		);
-
-		return words;
-	}
-
-	@GetMapping("/init")
-	public Set<String> wordleWords(HttpSession session) {
-		logger.info("Initializing to Wordle words");
-		Set<String> words = WordleService.wordleWords().getWords();
-		session.setAttribute("words", words);
-		return words;
+	@GetMapping("/s3")
+	public List<S3ObjectSummary> s3(HttpSession session) {
+		logger.info("Checking S3");
+		return awsService.listObjectSummaries(null);
+		// session.setAttribute("summaries", summaries);
 	}
 
 	private CanabrixAwsService awsService;
 	@Autowired
 	public void setAwsService(CanabrixAwsService awsService) {
 		this.awsService = awsService;
-	}
-
-	private WordleService wordleService;
-	@Autowired
-	public void setWordListService(WordleService wordleService) {
-		this.wordleService = wordleService;
 	}
 
 }
